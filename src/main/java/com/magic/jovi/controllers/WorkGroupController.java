4c6eaa5870cd86b327de7ed4dfc93e7df48682a6
Jovi <joyce221088@163.com>
2018-04-23 20:02:35 +0800

    加入swagger2的支持

--- src/main/java/com/magic/jovi/controllers/WorkGroupController.java
+++ src/main/java/com/magic/jovi/controllers/WorkGroupController.java
@@ -4,8 +4,13 @@ import com.magic.jovi.entities.vo.PageVO;
 import com.magic.jovi.entities.vo.WorkGroupVO;
 import com.magic.jovi.services.WorkGroupService;
 import com.magic.jovi.utils.ReqResult;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import springfox.documentation.annotations.ApiIgnore;
 
 /**
  * Created by fanjiawei on 2018/3/31
@@ -13,6 +18,7 @@ import org.springframework.web.bind.annotation.*;
  */
 @RequestMapping(value = "/workGroup")
 @RestController
+@Api(value = "工作组管理")
 public class WorkGroupController {
 
     private final WorkGroupService workGroupService;
@@ -28,7 +34,11 @@ public class WorkGroupController {
      * @return ReqResult
      */
     @RequestMapping(value = "/add")
-    public ReqResult<?> add(WorkGroupVO workGroupVO) {
+    @ApiImplicitParams({
+            @ApiImplicitParam(paramType = "query", name = "name", value = "工作组名称", required = true, dataType = "String")
+    })
+    @ApiOperation(value = "新增工作组", httpMethod = "POST", response = ReqResult.class)
+    public ReqResult<?> add(@ApiIgnore WorkGroupVO workGroupVO) {
         try {
             workGroupService.add(workGroupVO);
             return ReqResult.success("新增成功");
@@ -44,7 +54,11 @@ public class WorkGroupController {
      * @return ReqResult
      */
     @RequestMapping(value = "/edit")
-    public ReqResult<?> edit(WorkGroupVO workGroupVO) {
+    @ApiImplicitParams({
+            @ApiImplicitParam(paramType = "query", name = "name", value = "工作组名称", required = true, dataType = "String")
+    })
+    @ApiOperation(value = "编辑工作组", httpMethod = "POST", response = ReqResult.class)
+    public ReqResult<?> edit(@ApiIgnore WorkGroupVO workGroupVO) {
         try {
             workGroupService.edit(workGroupVO);
             return ReqResult.success("编辑成功");
@@ -60,6 +74,8 @@ public class WorkGroupController {
      * @return ReqResult
      */
     @RequestMapping(value = "/delete/{ids}")
+    @ApiImplicitParam(name = "ids", value = "需要删除的工作组id 批量删除时以逗号分隔", required = true, dataType = "String")
+    @ApiOperation(value = "删除工作组", httpMethod = "DELETE", response = ReqResult.class)
     public ReqResult<?> delete(@PathVariable String ids) {
         try {
             workGroupService.delete(ids);
@@ -76,6 +92,8 @@ public class WorkGroupController {
      * @return ReqResult
      */
     @RequestMapping(value = "/detail/{id}")
+    @ApiImplicitParam(name = "id", value = "工作组主键", required = true, dataType = "Long")
+    @ApiOperation(value = "工作组详情", httpMethod = "GET", response = ReqResult.class)
     public ReqResult<?> detail(@PathVariable Long id) {
         try {
             return ReqResult.success(workGroupService.detail(id));
@@ -91,6 +109,8 @@ public class WorkGroupController {
      * @return ReqResult
      */
     @RequestMapping(value = "/findAll")
+    @ApiImplicitParam(name = "page", value = "当前页码", required = true, dataType = "int")
+    @ApiOperation(value = "查询所有工作组，分页", httpMethod = "GET", response = ReqResult.class)
     public ReqResult<?> findAll(@RequestParam int page) {
         try {
             PageVO pageVO = new PageVO(page);
@@ -106,6 +126,7 @@ public class WorkGroupController {
      * @return ReqResult
      */
     @RequestMapping(value = "/findAllList", method = RequestMethod.GET)
+    @ApiOperation(value = "查询所有工作组，列表", httpMethod = "GET", response = ReqResult.class)
     public ReqResult<?> findAllList() {
         return ReqResult.success(workGroupService.findAllList());
     }
