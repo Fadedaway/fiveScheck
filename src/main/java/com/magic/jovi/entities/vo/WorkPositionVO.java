d12bbdd3d88feee089e003124bf786ca338d025d
Jovi <joyce221088@163.com>
2018-04-28 19:53:13 +0800

    增加机器与小组的关联 小组 1 ： n 机器

--- src/main/java/com/magic/jovi/entities/vo/WorkPositionVO.java
+++ src/main/java/com/magic/jovi/entities/vo/WorkPositionVO.java
@@ -9,11 +9,21 @@ public class WorkPositionVO extends BaseVO {
 
     private String name;
 
+    private Long groupId;
+
     public String getName() {
         return name;
     }
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public Long getGroupId() {
+        return groupId;
+    }
+
+    public void setGroupId(Long groupId) {
+        this.groupId = groupId;
     }
 }
